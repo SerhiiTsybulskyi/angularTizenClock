95212ea26069090621ced7b4b4baf79dfa8d31c2
SerhiiTsybulskyi <serhey.tsybulskyi@mev.com>
2019-06-10 10:43:31 +0300

    v1.0.0 - battery

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,18 +9,5 @@ declare var tizen: any;
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent implements OnInit {
-  ngOnInit(): void {
-    if (typeof tizen === 'undefined') { return; }
-    const healthinfoPermission = tizen.ppm.checkPermission('http://tizen.org/privilege/healthinfo');
-    const locationPermission = tizen.ppm.checkPermission('http://tizen.org/privilege/location');
-    switch (healthinfoPermission) {
-      case 'PPM_ASK':
-        tizen.ppm.requestPermission('http://tizen.org/privilege/healthinfo');
-    }
-
-    switch (locationPermission) {
-      case 'PPM_ASK':
-        tizen.ppm.requestPermission('http://tizen.org/privilege/location');
-    }
-  }
+  ngOnInit(): void {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,34 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { Ng2ArcProgressModule } from 'angular2-arc-progress';
-import { ConnectionServiceModule } from 'ng-connection-service';
-import { MomentModule } from 'ngx-moment';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { WatchFaceComponent } from './components/watch-face/watch-face.component';
 import { BatteryComponent } from './components/battery/battery.component';
-import { WeatherComponent } from './components/weather/weather.component';
-import { StepProgressComponent } from './components/step-progress/step-progress.component';
-import { RoundProgressModule } from 'angular-svg-round-progressbar';
-import { StepCounterComponent } from './components/step-counter/step-counter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WatchFaceComponent,
     BatteryComponent,
-    WeatherComponent,
-    StepProgressComponent,
-    StepCounterComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
-    RoundProgressModule,
     Ng2ArcProgressModule,
-    ConnectionServiceModule,
-    MomentModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
